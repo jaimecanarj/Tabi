d02c851807cc7dbbd92515ce3e46085549ca95b6
Jaime <jaimecanarj@gmail.com>
2024-10-15 20:41:38 +0200

    Añadida migración y modelo para lecturas

---
+++ database/migrations/2024_10_15_183110_create_lecturas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Kanji;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("lecturas", function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Kanji::class);
+            $table->string("lectura", length: 20);
+            $table->enum("tipo", ["onyomi", "kunyomi"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("lecturas");
+    }
+};
