3b40be8821af9f8bed83fc015fd24ecb1b422127
Jaime <jaimecanarj@gmail.com>
2024-10-16 10:39:58 +0200

    Añadido controlador para Kanji y tipo en lib/types

--- app/Http/Controllers/KanjiController.php
+++ app/Http/Controllers/KanjiController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kanji;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class KanjiController extends Controller
 {
-    //
+    public function index()
+    {
+        $kanjis = Kanji::all();
+        return Inertia::render("Kanjis", ["kanjis" => $kanjis]);
+    }
+
+    public function show($id)
+    {
+        $kanji = Kanji::find($id);
+        return Inertia::render("Kanji", ["kanji" => $kanji]);
+    }
 }
---
+++ resources/js/lib/types.ts
@@ -0,0 +1,9 @@
+type Kanji = {
+    id: number;
+    literal: string;
+    grado: number;
+    trazos: number;
+    frecuencia?: number;
+    created_at: Date;
+    updated_at: Date;
+};
