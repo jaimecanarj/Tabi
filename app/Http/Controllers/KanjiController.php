1c834c7fb3c1061fd0e78ce33f8544406383084d
Jaime <jaimecanarj@gmail.com>
2024-11-01 10:25:47 +0100

    Añadidos kanjis similares al modelo y controlador de kanjis

--- app/Http/Controllers/KanjiController.php
+++ app/Http/Controllers/KanjiController.php
@@ -19,11 +19,15 @@ class KanjiController extends Controller
         $kanji = Kanji::find($id);
         $lecturas = Kanji::find($id)->lecturas;
         $significados = Kanji::find($id)->significados;
+        $radicales = Kanji::find($id)->radicales;
+        $similares = Kanji::find($id)->similares;
 
         return Inertia::render("Kanji", [
             "kanji" => $kanji,
             "lecturas" => $lecturas,
             "significados" => $significados,
+            "radicales" => $radicales,
+            "similares" => $similares,
         ]);
     }
 }
--- app/Models/Kanji.php
+++ app/Models/Kanji.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Kanji extends Model
 {
@@ -18,5 +19,15 @@ class Kanji extends Model
     public function radicales()
     {
         return $this->belongsToMany(Radical::class);
+    }
+
+    public function similares(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Kanji::class,
+            "kanji_similar",
+            "kanji_id",
+            "kanji_similar_id"
+        );
     }
 }
