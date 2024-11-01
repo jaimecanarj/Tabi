086660574bdd67a4ad3e1efaa1ad8e4da8c25480
Jaime <jaimecanarj@gmail.com>
2024-11-01 10:31:20 +0100

    Creada vista detallada radical

--- app/Http/Controllers/RadicalController.php
+++ app/Http/Controllers/RadicalController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Radical;
+use Inertia\Inertia;
 
 class RadicalController extends Controller
 {
-    //
+    public function index()
+    {
+        $radicales = Radical::all();
+        return Inertia::render("Radicales", ["radicales" => $radicales]);
+    }
+
+    public function show($id)
+    {
+        $radical = Radical::find($id);
+        $kanjis = Radical::find($id)->kanjis;
+
+        return Inertia::render("Radical", [
+            "radical" => $radical,
+            "kanjis" => $kanjis,
+        ]);
+    }
 }
---
+++ resources/js/Pages/Radical.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import MainLayout from "@/Layouts/MainLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+import { Kanji, Radical } from "@/lib/types";
+import Button from "@/Components/ui/button/Button.vue";
+import ItemDetailsCard from "@/Components/ItemDetailsCard.vue";
+
+defineProps<{
+    radical: Radical;
+    kanjis: Kanji[];
+}>();
+</script>
+
+<template>
+    <Head :title="radical.literal" />
+    <MainLayout />
+    <main class="container mt-6">
+        <ItemDetailsCard
+            :literal="radical.literal"
+            tipo="radicales"
+            :titulo="radical.significado"
+        >
+            <!-- Trazos -->
+            <div class="mb-3">
+                <h3 class="inline mr-1 text-lg">Trazos:</h3>
+                <span class="text-lg font-semibold">{{ radical.trazos }}</span>
+            </div>
+            <!-- Kanjis donde aparece -->
+            <div class="mb-3">
+                <h3 class="mb-1 text-lg font-semibold tracking-tight">
+                    Kanjis donde aparece
+                </h3>
+                <div class="flex flex-wrap overflow-x-scroll max-h-96">
+                    <template v-for="kanji of kanjis">
+                        <Link :href="`/kanjis/${kanji.id}`">
+                            <Button class="m-1 text-2xl text-foreground">{{
+                                kanji.literal
+                            }}</Button>
+                        </Link>
+                    </template>
+                </div>
+            </div>
+        </ItemDetailsCard>
+    </main>
+</template>
