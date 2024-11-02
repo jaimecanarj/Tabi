f53dd7fa3c9445bb73016bb057cfbdc99b1d0022
Jaime <jaimecanarj@gmail.com>
2024-11-02 13:41:13 +0100

    Arreglado definitivamente links usuario mainLayout

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -42,26 +42,26 @@ import { Link } from "@inertiajs/vue3";
                             <Settings class="w-5 h-5 mr-1" /> Opciones
                         </DropdownMenuItem>
                         <DropdownMenuSeparator />
-                        <DropdownMenuItem class="cursor-pointer">
-                            <Link href="/logout" method="post" as="button">
+                        <Link href="/logout" method="post">
+                            <DropdownMenuItem class="cursor-pointer">
                                 <LogOut class="inline w-5 h-5 mr-1" />
                                 Cerrar sesión
-                            </Link>
-                        </DropdownMenuItem>
+                            </DropdownMenuItem>
+                        </Link>
                     </DropdownMenuContent>
                     <DropdownMenuContent align="end" class="w-40" v-else>
-                        <DropdownMenuItem class="cursor-pointer">
-                            <Link href="/login" as="button">
+                        <Link href="/login">
+                            <DropdownMenuItem class="cursor-pointer">
                                 <LogIn class="inline w-5 h-5 mr-1" />
                                 Iniciar sesión
-                            </Link>
-                        </DropdownMenuItem>
-                        <DropdownMenuItem class="cursor-pointer">
-                            <Link href="/register" as="button">
+                            </DropdownMenuItem>
+                        </Link>
+                        <Link href="/register">
+                            <DropdownMenuItem class="cursor-pointer">
                                 <UserPlus class="inline w-5 h-5 mr-1" />
                                 Registrarse
-                            </Link>
-                        </DropdownMenuItem>
+                            </DropdownMenuItem>
+                        </Link>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
