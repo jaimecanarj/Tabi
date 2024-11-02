afcd7afd6e89fb716e3f8bb7f410d61a4f1c72d0
Jaime <jaimecanarj@gmail.com>
2024-11-02 22:54:25 +0100

    Actualizado cambio de tema para funcionar con el botón en lugar del icono

--- resources/js/Components/Navbar/ToggleDarkMode.vue
+++ resources/js/Components/Navbar/ToggleDarkMode.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
-import { Button } from "@/Components/ui/button";
+import { computed } from "vue";
 import { useColorMode } from "@vueuse/core";
 import { Sun, Moon } from "lucide-vue-next";
-import { computed } from "vue";
+import { Button } from "@/Components/ui/button";
 
 const { system, store } = useColorMode({ disableTransition: false });
 const mode = computed(() =>
     store.value === "auto" ? system.value : store.value,
 );
+
+const changeTheme = () => {
+    store.value = store.value == "light" ? "dark" : "light";
+};
 </script>
 
 <template>
-    <Button variant="ghost" size="icon">
+    <Button variant="ghost" size="icon" @click="changeTheme">
         <Sun
-            @click="store = 'light'"
             class="absolute transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100"
         />
         <Moon
-            @click="store = 'dark'"
             class="transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0"
         />
     </Button>
