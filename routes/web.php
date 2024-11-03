05ae424d3e239aab7ee501739ecd04caa97816fc
Jaime <jaimecanarj@gmail.com>
2024-11-03 17:56:13 +0100

    Eliminado editprofile de breeze

--- routes/web.php
+++ routes/web.php
@@ -27,17 +27,4 @@ Route::get("/historia/{user_id}/{kanji_id}", [
 Route::post("/historia", [HistoriaController::class, "store"]);
 Route::delete("/historia", [HistoriaController::class, "destroy"]);
 
-// Rutas legacy
-Route::middleware("auth")->group(function () {
-    Route::get("/profile", [ProfileController::class, "edit"])->name(
-        "profile.edit"
-    );
-    Route::patch("/profile", [ProfileController::class, "update"])->name(
-        "profile.update"
-    );
-    Route::delete("/profile", [ProfileController::class, "destroy"])->name(
-        "profile.destroy"
-    );
-});
-
 require __DIR__ . "/auth.php";
