e20febd892ef2d7f637c1a4f1170b599fd7df164
Jaime <jaimecanarj@gmail.com>
2024-11-10 16:44:39 +0100

    Vista provisional de home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,52 +1,55 @@
 <script setup lang="ts">
 import MainLayout from "@/Layouts/MainLayout.vue";
-import { Head } from "@inertiajs/vue3";
-import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardContent,
-    CardFooter,
-} from "@/Components/ui/card";
+import { Head, Link } from "@inertiajs/vue3";
 import Button from "@/Components/ui/button/Button.vue";
-import { BookOpen, GraduationCap } from "lucide-vue-next";
+import { Badge } from "@/Components/ui/badge";
 </script>
 
 <template>
     <Head title="Home" />
     <MainLayout />
     <main class="container mt-12">
-        <section
-            class="flex flex-col items-center space-y-4 md:space-x-4 md:space-y-0 md:flex-row"
-        >
-            <Card
-                class="flex flex-col justify-between min-w-96 md:w-1/2 h-96 bg-muted"
-            >
-                <CardHeader>
-                    <CardTitle class="text-4xl text-center">
-                        <GraduationCap class="inline w-10 h-10 mr-2" />
-                        Repasar
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>Como hago para memorizar todo eso.</CardContent>
-                <CardFooter>
-                    <Button class="w-full"> Empezar </Button>
-                </CardFooter>
-            </Card>
-            <Card
-                class="flex flex-col justify-between min-w-96 h-96 md:w-1/2 bg-secondary"
-            >
-                <CardHeader>
-                    <CardTitle class="text-4xl text-center">
-                        <BookOpen class="inline w-8 h-8 mr-2" />
-                        Aprender
-                    </CardTitle>
-                </CardHeader>
-                <CardContent>Tocará empezar por algún lado.</CardContent>
-                <CardFooter>
-                    <Button class="w-full" variant="default">Empezar</Button>
-                </CardFooter>
-            </Card>
+        <section class="flex gap-5">
+            <div class="p-5 rounded-md shadow-lg bg-primary w-96">
+                <h2 class="mb-3 text-3xl font-semibold text-primary-foreground">
+                    Estudiar
+                    <Badge variant="secondary" class="text-base">10</Badge>
+                </h2>
+                <p class="text-primary-foreground">
+                    Hoy has estudiado
+                    <span class="text-lg font-semibold underline">10</span>
+                    kanjis
+                </p>
+                <Link href="/repasar">
+                    <Button
+                        class="w-full mt-2 bg-secondary hover:bg-secondary-foreground/90 text-primary"
+                        >Empezar</Button
+                    >
+                </Link>
+            </div>
+            <div class="p-5 rounded-md shadow-lg bg-primary-alt w-96">
+                <h2 class="mb-3 text-3xl font-semibold text-primary-foreground">
+                    Repasar
+                    <Badge
+                        variant="secondary"
+                        class="justify-center text-base min-w-10"
+                    >
+                        6
+                    </Badge>
+                </h2>
+                <p class="text-primary-foreground">
+                    Hoy has repasado
+                    <span class="text-lg font-semibold underline">15</span>
+                    kanjis
+                </p>
+                <Link href="/estudiar">
+                    <Button
+                        class="w-full mt-2 bg-secondary hover:bg-secondary-foreground/90 text-primary-alt"
+                    >
+                        Empezar
+                    </Button>
+                </Link>
+            </div>
         </section>
         <section>Últimos kanjis estudiados 人</section>
         <section>
