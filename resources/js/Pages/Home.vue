7b9c2bb6d85630645b86f614c49c3e24bf242ad2
Jaime <jaimecanarj@gmail.com>
2024-12-05 19:04:57 +0100

    Añadida vista usuario no registrado a home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Head } from "@inertiajs/vue3";
+import { Head, usePage } from "@inertiajs/vue3";
 import MainLayout from "@/Layouts/MainLayout.vue";
 import HomeStudyCard from "@/Components/Home/HomeStudyCard.vue";
 import HomeLastStudy from "@/Components/Home/HomeLastStudy.vue";
@@ -8,6 +8,8 @@ import HomeProgress from "@/Components/Home/HomeProgress.vue";
 import HomeNextReview from "@/Components/Home/HomeNextReview.vue";
 import { Estudio, Kanji } from "@/lib/types";
 import { momentLocale } from "@/lib/utils";
+
+const page = usePage();
 
 momentLocale();
 
@@ -18,7 +20,27 @@ defineProps<{ estudios: (Estudio & { kanji: Kanji })[] }>();
     <Head title="Home" />
     <MainLayout />
     <main class="container my-12">
-        <div class="grid lg:grid-cols-3">
+        <!--Vista invitado-->
+        <div v-if="!page.props.auth.user" class="flex flex-col items-center">
+            <div class="flex flex-col">
+                <div
+                    class="w-[300px] rounded-lg bg-card p-3 text-xl sm:ml-36 sm:w-[400px] sm:text-2xl md:w-[500px] md:text-3xl"
+                >
+                    Inicia sesión o registrate<br />
+                    para ver tus estudios y progreso.
+                </div>
+                <div
+                    class="ml-[104px] inline-block h-0 w-0 border-b-0 border-l-[24px] border-r-[24px] border-t-[40px] border-solid border-b-transparent border-l-transparent border-r-transparent border-t-card sm:ml-[168px]"
+                ></div>
+                <img
+                    src="/assets/Viajero.webp"
+                    alt="Viajero"
+                    class="mt-2 w-64 sm:w-96"
+                />
+            </div>
+        </div>
+        <!--Vista usuario registrado-->
+        <div v-else class="grid lg:grid-cols-3">
             <div class="lg:col-span-2 lg:mx-5">
                 <section
                     class="flex flex-col justify-between gap-5 xl:flex-row"
