4397a0747834195da484604ad729a298f47b7314
Jaime <jaimecanarj@gmail.com>
2024-12-08 13:05:25 +0100

    Añadida vista de error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,21 +3,48 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . "/../routes/web.php",
+        commands: __DIR__ . "/../routes/console.php",
+        health: "/up"
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\HandleInertiaRequests::class,
-            \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
-        ]);
+        $middleware->web(
+            append: [
+                \App\Http\Middleware\HandleInertiaRequests::class,
+                \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            ]
+        );
 
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->respond(function (
+            Response $response,
+            Throwable $exception,
+            Request $request
+        ) {
+            if (
+                !app()->environment(["local", "testing"]) &&
+                in_array($response->getStatusCode(), [500, 503, 404, 403])
+            ) {
+                return Inertia::render("Error", [
+                    "status" => $response->getStatusCode(),
+                ])
+                    ->toResponse($request)
+                    ->setStatusCode($response->getStatusCode());
+            } elseif ($response->getStatusCode() === 419) {
+                return back()->with([
+                    "message" => "La página expiró. Inténtalo de nuevo.",
+                ]);
+            }
+
+            return $response;
+        });
+    })
+    ->create();
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Head, Link } from "@inertiajs/vue3";
+import { useColorMode } from "@vueuse/core";
+import { Button } from "@/Components/ui/button";
+
+defineProps<{ status: string }>();
+
+useColorMode();
+</script>
+
+<template>
+    <Head :title="status" />
+    <main
+        class="container box-border flex h-screen flex-col items-center justify-between gap-4 pt-12"
+    >
+        <h2 class="text-8xl font-semibold">{{ status }}</h2>
+        <Link href="/inicio"
+            ><Button size="lg" class="w-80">Volver a inicio</Button></Link
+        >
+        <img src="/assets/Viajero%20404.webp" alt="404" class="w-[50vw]" />
+    </main>
+</template>
